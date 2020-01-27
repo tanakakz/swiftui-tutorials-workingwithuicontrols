46b87761ffd9e7881440eadb6c6120a1261cef82
tanakakz <tanakakz.feb.23@gmail.com>
2020-01-27 14:43:56 +0900

    add profile summary view

---
+++ Landmarks/Landmarks/Profile/ProfileSummary.swift
@@ -0,0 +1,40 @@
+//
+//  ProfileSummary.swift
+//  Landmarks
+//
+//  Created by tkz on 2020/01/27.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileSummary: View {
+    var profile: Profile
+    
+    static let goalFormat: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    
+    var body: some View {
+        List {
+            Text(profile.username)
+                .bold()
+                .font(.title)
+            
+            Text("Notifications: \(self.profile.prefersNotifications ? "On" : "Off" )")
+            
+            Text("Seasonal Photos: \(self.profile.seasonalPhoto.rawValue)")
+            
+            Text("Goal Date: \(self.profile.goalDate, formatter: Self.goalFormat)")
+        }
+    }
+}
+
+struct ProfileSummary_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileSummary(profile: Profile.default)
+    }
+}
